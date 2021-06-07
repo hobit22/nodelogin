a2b32b83259eb4ce3667f01bfc45fa27cc1ac1e8
hobit22 <hobit22@naver.com>
2021-06-07 12:16:17 +0900

    멤버 라우터 초기 시작

--- routes/member.js
+++ routes/member.js
@@ -1,7 +1,32 @@
 const express = require('express');
 const router = express.Router;
 
-
+/** /member/join */
+router.route("/join")
+		/**	 회원가입 양식 */
+		.get((req,res,next) =>{
+			
+		})
+		/**	 회원가입 처리 */
+		.post((req,res,next)=>{
+			
+		});
+		
+/** /member/login */
+router.route("/login")
+		/** 로그인 양식 */
+		.get((req,res,next) =>{
+			
+		})
+		/** 로그인 처리 */ 
+		.post((req,res,next)=>{
+			
+		});
+		
+/** /member/logout */
+router.get("/logout", (req,res,next) =>{
+	
+});
 
 
 
